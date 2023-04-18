97f2f21c5b15fe3c67daf74ef10123d375bec4e3
wuyuetao <wuyuetao@bilibili.com>
2023-04-18 15:04:54 +0800

    feat:显式/隐式intent例子

--- app/src/main/java/com/example/activitytest/MainActivity.kt
+++ app/src/main/java/com/example/activitytest/MainActivity.kt
@@ -10,21 +10,33 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
-        val btn: Button = findViewById(R.id.button)
-        btn.setOnClickListener {
+        val btn1: Button = findViewById(R.id.main_btn1)
+        btn1.setOnClickListener {
             doDynamicIntent()
+        }
+
+        val btn2: Button = findViewById(R.id.main_btn2)
+        btn2.setOnClickListener {
             doStaticIntent()
         }
     }
 
+    // 显式intent，通常应用在自己程序中
     private fun doDynamicIntent() {
         val intent = Intent()
-        intent.setClass(this, ShareActivity2::class.java)
+        intent.setClass(this, ShareActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        intent.putExtra("dataTest", "hello world")
         startActivity(intent)
     }
 
+    // 隐式intent，筛选出可响应此intent(action, category, data)的组件
     private fun doStaticIntent() {
         val intent = Intent("actionTest")
+        val bundle = Bundle()
+        bundle.putString("name", "Garel")
+        bundle.putInt("height", 170)
+        intent.putExtras(bundle)
         startActivity(intent)
     }
 }
--- app/src/main/java/com/example/activitytest/ShareActivity2.kt
+++ app/src/main/java/com/example/activitytest/ShareActivity2.kt
@@ -6,6 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 class ShareActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_share)
+        setContentView(R.layout.activity_share_2)
+
+        val intent = intent
+        val bundle = intent.extras
+        val name = bundle?.getString("name")
+        val height = bundle?.getInt("height")
     }
 }
